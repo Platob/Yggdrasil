bbb5e62af16b8c5027202d1ab3c194e89f675477
Nicolas FILLOT <CAL1170514@admin.hubtotal.net>
2026-05-03 12:48:55 +0200

    0.7.23: session spark http

--- python/src/yggdrasil/version.py
+++ python/src/yggdrasil/version.py
@@ -39,5 +39,5 @@ class VersionInfo(NamedTuple):
         return base
 
 
-__version_info__ = VersionInfo.from_string("0.7.22")
+__version_info__ = VersionInfo.from_string("0.7.23")
 __version__ = str(__version_info__)
