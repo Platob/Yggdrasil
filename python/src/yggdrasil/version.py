bc0c3df4157c058fc89161455fbe5c690e46ba5d
Nicolas FILLOT <CAL1170514@admin.hubtotal.net>
2026-05-03 10:02:10 +0200

    0.7.20: fix code

--- python/src/yggdrasil/version.py
+++ python/src/yggdrasil/version.py
@@ -39,5 +39,5 @@ class VersionInfo(NamedTuple):
         return base
 
 
-__version_info__ = VersionInfo.from_string("0.7.19")
+__version_info__ = VersionInfo.from_string("0.7.20")
 __version__ = str(__version_info__)
